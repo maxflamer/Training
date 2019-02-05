54a4868dff591c2be3d1b562e9835f37437e3c3b
maxflamer <km_95@hotmail.com>
2019-02-05 18:16:23 +0530

    Add project files.

---
+++ SignalR/Controllers/LetsChatController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+
+namespace SignalR.Controllers
+{
+    public class LetsChatController : Controller
+    {
+        // GET: LetsChat
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LetsChat()
+        {
+            return View();
+        }
+
+       
+    }
+}
---
+++ SignalR/Hubs/LetsChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Web.Optimization;
+
+namespace LetsChatApplication.Hubs
+{
+    public class LetsChatHub : Hub
+    {
+        public void Send(string name, string message, string connId)
+        {
+            Clients.Client(connId).addNewMessageToPage(name, message);
+        }
+    }
+}
